2e17abb3d44d09c667b253b8b3bcdb9f9ebdc696
James Perdue <JTPerdue525@gmail.com>
2023-01-03 19:26:58 -0500

    Added Questions

--- src/components/data.js
+++ src/components/data.js
@@ -1,29 +1,93 @@
 export default[
     {
         id:1,
-        question:"How many Overwatch games are there?", 
-        a:"1",
-        b:"2",
-        c:"3",
-        d:"4 or more",
-        correctAnswer: "b"
+        question:"How many ghosts visit Mr. Scrooge in the book, 'A Christmas Carol' by Charles Dickens?", 
+        a:"2",
+        b:"3",
+        c:"4",
+        d:"5 or more",
+        correctAnswer: "c"
     },
     {
         id:2,
-        question:"What year was Hearthstone released?", 
-        a:"2011",
-        b:"2015",
-        c:"2014",
-        d:"2018",
+        question:"What movie is the expression 'I see dead people' from?", 
+        a:"The Shining",
+        b:"It",
+        c:"The Sixth Sense",
+        d:"Clockwork Orange",
         correctAnswer: "c"
     },
     {
         id:3,
-        question:"What game company created Overwatch and Hearthstone?", 
-        a:"Blizzard",
-        b:"Epic Games",
-        c:"EA",
-        d:"Ubisoft",
+        question:"What is Batman's name?", 
+        a:"Bruce Wayne",
+        b:"Bryce Wayne",
+        c:"John Wayne",
+        d:"Charlie Wayne",
+        correctAnswer: "a"
+    },
+    {
+        id:4,
+        question:"What was the name of the tow truck in 'Cars' the movie?", 
+        a:"Guido",
+        b:"Tow Mater",
+        c:"Tow Truck",
+        d:"Doc",
+        correctAnswer: "b"
+    },
+    {
+        id:5,
+        question:"In the movie 'Finding Nemo', where was Nemo taken?", 
+        a:"Melbourne, Australia",
+        b:"Canberra, Australia",
+        c:"Perth, Australia",
+        d:"Sydney, Australia",
+        correctAnswer: "d"
+    },
+    {
+        id:6,
+        question:"Where did Harry Potter go to school?", 
+        a:"Hogwarts",
+        b:"Gryffindor",
+        c:"Hogsmeade",
+        d:"Oxford",
+        correctAnswer: "a"
+    },
+    {
+        id:7,
+        question:"What type of monster is Shrek?", 
+        a:"Dwarf",
+        b:"Ogre",
+        c:"Giant",
+        d:"Elf",
+        correctAnswer: "b"
+    },
+    {
+        id:8,
+        question:"Where was Nemo taken in 'Finding Nemo'?", 
+        a:"Melbourne, Australia",
+        b:"Canberra, Australia",
+        c:"Perth, Australia",
+        d:"Sydney, Australia",
+        correctAnswer: "d"
+    },
+    {
+        id:9,
+        question:"What hero in DC universe is very similar to the Greek God Poseidon?", 
+        a:"Batman",
+        b:"Green Lantern",
+        c:"Aqua Man",
+        d:"Superman",
+        correctAnswer: "c"
+    },
+    {
+        id:10,
+        question:"What color is the road that Dorothy follows in the Wizard of Oz?", 
+        a:"Yellow",
+        b:"Red",
+        c:"Orange",
+        d:"Black",
         correctAnswer: "a"
     }
+    
 ]
