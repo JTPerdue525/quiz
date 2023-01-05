39e942aead65aa0407bc6c57a458e472959caa55
James Perdue <JTPerdue525@gmail.com>
2023-01-05 18:43:08 -0500

    Added response message after answering question

--- src/components/Question.js
+++ src/components/Question.js
@@ -1,4 +1,5 @@
-import React from "react"
+import React, { useState } from "react"
+import "./quiz.css"
 
 export default function Question(props){
 
@@ -7,6 +8,7 @@ export default function Question(props){
     let bChecker = false;
     let cChecker = false;
     let dChecker = false;
+    let [response, setResponse] = useState('None');
 
     //Definitely a better way to do this, but this will work for now
     if(answer=='a'){
@@ -24,23 +26,45 @@ export default function Question(props){
 
     function correct(checker){
         if(checker==true){
-            console.log("This is the right answer")
+            //console.log("This is the right answer")
+            setResponse('Correct')
         }
         else{
-            console.log("This is the wrong answer")
+            //console.log("This is the wrong answer")
+            setResponse('Incorrect')
         }
     }
 
-
-    return(
-        <div className="question">
-            <h2>{props.question}</h2>
-            <div className="question-choices">
-                <button className="choice-1" type='button' onClick={() => {correct(aChecker)}}>A: {props.a}</button>
-                <button className="choice-2" type='button' onClick={() => {correct(bChecker)}}>B: {props.b}</button>
-                <button className="choice-3" type='button' onClick={() => {correct(cChecker)}}>C: {props.c}</button>
-                <button className="choice-4" type='button' onClick={() => {correct(dChecker)}}>D: {props.d}</button>
+    //Initially displays the questions and answers normally
+    if(response=="None"){
+        return(
+            <div className="question">
+                <h2>{props.question}</h2>
+                <div className="question-choices">
+                    <button className="choice-1" type='button' onClick={() => {correct(aChecker)}}>A: {props.a}</button>
+                    <button className="choice-2" type='button' onClick={() => {correct(bChecker)}}>B: {props.b}</button>
+                    <button className="choice-3" type='button' onClick={() => {correct(cChecker)}}>C: {props.c}</button>
+                    <button className="choice-4" type='button' onClick={() => {correct(dChecker)}}>D: {props.d}</button>
+                </div>
             </div>
-        </div>
-    )
+        )
+    }
+    //Displays question and a green correct response
+    else if(response=="Correct"){
+        return(
+            <div className="response">
+                <h2>{props.question}</h2>
+                <h2 className="response-message1">Correct</h2>
+            </div>
+        )
+    }
+    //Displays question and a red incorrect response
+    else if(response=="Incorrect"){
+        return(
+            <div className="response">
+                <h2>{props.question}</h2>
+                <h2 className="response-message2">Incorrect</h2>
+            </div>
+        )
+    }
 }
