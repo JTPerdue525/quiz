fc70fcc5591aaaa397f5a3eb05ec7fafc7c01bbd
James Perdue <JTPerdue525@gmail.com>
2023-01-12 18:53:54 -0500

    Added and modified questions

--- src/components/data.js
+++ src/components/data.js
@@ -64,12 +64,12 @@ export default[
     },
     {
         id:8,
-        question:"Where was Nemo taken in 'Finding Nemo'?", 
-        a:"Melbourne, Australia",
-        b:"Canberra, Australia",
-        c:"Perth, Australia",
-        d:"Sydney, Australia",
-        correctAnswer: "d"
+        question:"What is the name of Japanese cartoons?", 
+        a:"Cartoons",
+        b:"Manga",
+        c:"Anime",
+        d:"Shounen",
+        correctAnswer: "c"
     },
     {
         id:9,
@@ -88,6 +88,96 @@ export default[
         c:"Orange",
         d:"Black",
         correctAnswer: "a"
+    },
+    {
+        id:11,
+        question:"What genre of music is 'The Weeknd' best known for?", 
+        a:"Rap",
+        b:"Pop",
+        c:"R&B",
+        d:"EDM",
+        correctAnswer: "b"
+    },
+    {
+        id:12,
+        question:"What does the term 'NFL' stand for?", 
+        a:"National Foosball League",
+        b:"National Football League",
+        c:"Natural Forest Land",
+        d:"Nested For Loop",
+        correctAnswer: "b"
+    },
+    {
+        id:13,
+        question:"What social media platform uses a bird as it's logo?", 
+        a:"Instagram",
+        b:"Facebook",
+        c:"Tik Tok",
+        d:"Twitter",
+        correctAnswer: "d"
+    },
+    {
+        id:14,
+        question:"What music artist goes by the name Ye?", 
+        a:"Kendrick Lamar",
+        b:"Eminem",
+        c:"Kanye West",
+        d:"Yeat",
+        correctAnswer: "c"
+    },
+    {
+        id:15,
+        question:"What video game franchise has a vehicle called a Banshee?", 
+        a:"Halo",
+        b:"Call of Duty",
+        c:"Star Wars Battlefront",
+        d:"God of War",
+        correctAnswer: "a"
+    },
+    {
+        id:16,
+        question:"Who was the actor who played as Forrest Gump in the movie, 'Forrest Gump'?", 
+        a:"Tom Cruise",
+        b:"Tom Holland",
+        c:"Tom Hiddleston",
+        d:"Tom Hanks",
+        correctAnswer: "d"
+    },
+    {
+        id:17,
+        question:"What movie is the Netflix series, 'Cobra Kai', a sequel of?", 
+        a:"Snakes on A Plane",
+        b:"Karate Kid",
+        c:"Saving Private Ryan",
+        d:"Fight Club",
+        correctAnswer: "b"
+    },
+    {
+        id:18,
+        question:"Which music legend wrote the song, 'Tangled Up in Blue'?", 
+        a:"Bob Dylan",
+        b:"John Lennon",
+        c:"Queen",
+        d:"Elton John",
+        correctAnswer: "a"
+    },
+    {
+        id:19,
+        question:"What state was most of the show, 'The Walking Dead', filmed?", 
+        a:"Alabama",
+        b:"California",
+        c:"Georgia",
+        d:"Florida",
+        correctAnswer: "c"
+    },
+    {
+        id:20,
+        question:"What sport does Lebron James play?", 
+        a:"Hockey",
+        b:"Basketball",
+        c:"Football",
+        d:"Soccer",
+        correctAnswer: "b"
     }
     
 ]
